e8fddc46e1d34269d187f5062f35ff464142b6b1
KNKNT <kizilov.kiza573@gmail.com>
2026-02-19 18:10:23 +0300

    Добавьте файлы проекта.

---
+++ ChildPrison/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ChildPrison
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e) => DragMove();
+        private void Button_Click(object sender, RoutedEventArgs e) => Close();
+        private void Button_Click_2(object sender, RoutedEventArgs e) => WindowState = WindowState.Minimized;
+        private void Button_Click_1(object sender, RoutedEventArgs e) =>
+            WindowState = WindowState == WindowState.Maximized
+                ? WindowState.Normal
+                : WindowState.Maximized;
+
+    }
+}
---
+++ ChildPrison/Registration.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ChildPrison
+{
+    /// <summary>
+    /// Логика взаимодействия для Registration.xaml
+    /// </summary>
+    public partial class Registration : Window
+    {
+        public Registration()
+        {
+            InitializeComponent();
+        }
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e) => DragMove();
+        private void Button_Click(object sender, RoutedEventArgs e) => Close();
+        private void Button_Click_2(object sender, RoutedEventArgs e) => WindowState = WindowState.Minimized;
+    }
+}
