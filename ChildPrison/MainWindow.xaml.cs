2953a1d22ec3559cc2043fe71f8648dcfd05882c
KNKNT <kizilov.kiza573@gmail.com>
2026-03-31 02:00:40 +0300

    Add ListPage

--- ChildPrison/MainWindow.xaml.cs
+++ ChildPrison/MainWindow.xaml.cs
@@ -1,11 +1,16 @@
 ﻿using System.Windows;
 using System.Windows.Input;
+using diplomApp.View.ListPage;
 
 namespace ChildPrison
 {
     public partial class MainWindow : Window
     {
-        public MainWindow() => InitializeComponent();
+        public MainWindow()
+        {
+            InitializeComponent();
+            PageFrame.Content = new ListPage();
+        }
         private void Border_MouseDown(object sender, MouseButtonEventArgs e) => DragMove();
         private void Button_Click(object sender, RoutedEventArgs e) => Close();
         private void Button_Click_2(object sender, RoutedEventArgs e) => WindowState = WindowState.Minimized;
---
+++ ChildPrison/View/ListPage/Card.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace diplomApp.View.ListPage
+{
+    public partial class Card : UserControl
+    {
+        private SolidColorBrush[] _brushes =
+        {
+            (SolidColorBrush)new BrushConverter().ConvertFromString("#ffa6a6"), //red
+            (SolidColorBrush)new BrushConverter().ConvertFromString("#fff2a6"), //yellow
+            (SolidColorBrush)new BrushConverter().ConvertFromString("#f6a6ff"), //pink
+            (SolidColorBrush)new BrushConverter().ConvertFromString("#a6ffc3"), //green
+            (SolidColorBrush)new BrushConverter().ConvertFromString("#a6ffff")  //blue
+        };
+        public Card()
+        {
+            InitializeComponent();
+
+            Random random = new Random();
+            ValueBorder.Background = _brushes[random.Next(0, _brushes.Length)];
+        }
+       
+        //Caption property
+        public static readonly DependencyProperty BoxCaptionProperty =
+           DependencyProperty.Register(nameof(BoxCaption), typeof(string), typeof(Card),
+               new PropertyMetadata("Описание"));
+
+        public string BoxCaption
+        {
+            get => (string)GetValue(BoxCaptionProperty);
+            set => SetValue(BoxCaptionProperty, value);
+        }
+
+        //Value property
+        public static readonly DependencyProperty BoxValueProperty =
+            DependencyProperty.Register(nameof(BoxValue), typeof(string), typeof(Card),
+                new PropertyMetadata("Значение"));
+
+        public string BoxValue
+        {
+            get => (string)GetValue(BoxValueProperty);
+            set => SetValue(BoxValueProperty, value);
+        }
+
+        public static readonly DependencyProperty IsReadOnlyProperty =
+            DependencyProperty.Register(nameof(IsReadOnly), typeof(bool), typeof(Card),
+                new PropertyMetadata(true));
+
+        public bool IsReadOnly
+        {
+            get => (bool)GetValue(IsReadOnlyProperty);
+            set => SetValue(IsReadOnlyProperty, value);
+        }
+    }
+}
